5abecd1bd307bbc731b96b3fdfb0fc936368c758
sunbeam891 <shubhamparth1@gmail.com>
2021-09-03 16:15:55 +1000

    Updated scripts_main

--- Scripts_main/Build_all.sh
+++ Scripts_main/Build_all.sh
@@ -7,12 +7,12 @@ cd $PFBENCH
 
 cd Scripts_main
 
-chmod +x requirements_install.sh
+chmod +x Requirements_install.sh
 
 ./requirements_install.sh
 
-docker pull saddie/confuzzius:script_edit_4
+sudo docker pull saddie/confuzzius:script_edit_4
 
-docker pull saddie/ilf:script_edit_4
+sudo docker pull saddie/ilf:script_edit_4
 
-docker pull saddie/sfuzz_build:latest
+sudo docker pull saddie/sfuzz_build:latest
