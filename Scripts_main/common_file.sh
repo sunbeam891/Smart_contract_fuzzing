384733e77145276e05c1209c263618b84463d836
Shubham Parth <shubhamparth1@gmail.com>
2021-10-18 03:40:55 +1100

    Updated common.sh for "all" runs

--- Scripts_main/common_file.sh
+++ Scripts_main/common_file.sh
@@ -11,13 +11,48 @@ cids=()
 excelnames='Start'
 outputnames='Start'
 #create one container for each run
-for i in $(seq 1 $RUNS); do
-  id=$(docker run --cpus=1 -v ${TARGET}:${ROOT}dataset -d -it --entrypoint='' $DOCIMAGE /bin/bash -c "cd ${ROOT}dataset/Scripts_main && chmod +x fuzzer_run.sh && ./fuzzer_run.sh ${FUZZER} ${ROOT}dataset ${ROOT} ${i} ${SAVETO}") # i used for determining container number for output file name 
-  cids+=(${id::12}) #store only the first 12 characters of a container ID
-  excelnames+=',${FUZZER}-${i}-output'
-  outputnames+=',${SAVETO}_${i}'
+if [[ $FUZZER != "all" ]]
+then
+  for i in $(seq 1 $RUNS); do
+    id=$(docker run --cpus=1 -v ${TARGET}:${ROOT}dataset -d -it --entrypoint='' $DOCIMAGE /bin/bash -c "cd ${ROOT}dataset/Scripts_main && chmod +x fuzzer_run.sh && ./fuzzer_run.sh ${FUZZER} ${ROOT}dataset ${ROOT} ${i} ${SAVETO}") # i used for determining container number for output file name 
+    cids+=(${id::12}) #store only the first 12 characters of a container ID
+    excelnames+=,${FUZZER}-${i}-output
+    outputnames+=,${SAVETO}_${i}
 
-done
+  done
+fi
+
+if [[ $FUZZER == "all" ]]
+then 
+  for i in $(seq 1 $RUNS); do
+    id=$(docker run --cpus=1 -v ${TARGET}:/home/sFuzz/dataset -d -it --entrypoint='' saddie/sFuzz:script_edit_5 /bin/bash -c "cd /home/sFuzz/dataset/Scripts_main && chmod +x fuzzer_run.sh && ./fuzzer_run.sh sFuzz /home/sFuzz/dataset /home/sFuzz/ ${i} results-sFuzz") # i used for determining container number for output file name 
+    cids+=(${id::12}) #store only the first 12 characters of a container ID
+    excelnames+=,sFuzz-${i}-output
+    outputnames+=,results-sFuzz_${i}
+
+  done
+fi
+if [[ $FUZZER == "all" ]]
+then 
+  for i in $(seq 1 $RUNS); do
+    id=$(docker run --cpus=1 -v ${TARGET}:/go/src/ilf/dataset -d -it --entrypoint='' saddie/ILF:script_edit_6 /bin/bash -c "cd /go/src/ilf/dataset/Scripts_main && chmod +x fuzzer_run.sh && ./fuzzer_run.sh ILF /go/src/ilf/dataset /go/src/ilf/ ${i} results-ILF") # i used for determining container number for output file name 
+    cids+=(${id::12}) #store only the first 12 characters of a container ID
+    excelnames+=,ILF-${i}-output
+    outputnames+=,results-ILF_${i}
+
+  done
+fi
+
+if [[ $FUZZER == "all" ]]
+then 
+  for i in $(seq 1 $RUNS); do
+    id=$(docker run --cpus=1 -v ${TARGET}:/root/dataset -d -it --entrypoint='' saddie/confuzzius:script_edit_6 -c "cd /root/dataset/Scripts_main && chmod +x fuzzer_run.sh && ./fuzzer_run.sh Confuzzius /root/dataset /root/ ${i} results-Confuzzius") # i used for determining container number for output file name 
+    cids+=(${id::12}) #store only the first 12 characters of a container ID
+    excelnames+=,Confuzzius-${i}-output
+    outputnames+=,results-Confuzzius_${i}
+
+  done
+fi
 
 dlist="" #docker list
 for id in ${cids[@]}; do
@@ -37,7 +72,7 @@ rid=$(python3 ${TARGET}/Scripts_main/Result_present.py ${excelnames} ${outputnam
 wait $rid
 
 
-printf "\n${FUZZER^^}: Collected results and saved them to ${SAVETO}"
+printf "\n${FUZZER^^}: Collected results and saved them to ${TARGET}/Final_results/"
 
 
 printf "\n${FUZZER^^}: I am done!"
